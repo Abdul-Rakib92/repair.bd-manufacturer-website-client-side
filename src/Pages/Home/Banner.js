b4c52557c9705ec1179df968cd0f4486cf814f8e
Abdul-Rakib92 <rakibctgu@gmail.com>
2022-05-22 21:41:29 +0600

    banner added

---
+++ src/Pages/Home/Banner.js
@@ -0,0 +1,26 @@
+import React from "react";
+import banner from '../../assets/images/banner.jpg'
+
+const Banner = () => {
+  return (
+    <div>
+      <div class="hero min-h-screen ">
+        <div class="hero-content flex-col lg:flex-row-reverse">
+          <img
+            src={banner}
+            class="max-w-xl rounded-lg shadow-2xl"
+          />
+          <div class="mr-5">
+            <h1 class="text-5xl font-bold"> <span class="text-orange-500">Wellcome To Repair. <span class="text-accent">BD</span></span></h1>
+            <p class="py-6 text-xl">
+            Repair.BD has been selling power tools in Bangladesh for the last 10 years. We have industrial pressure washers, automatic drill machine & battery drill machine. We also have rotary hammer drill machines, bosch drill machine. But wait, there is more, demolition hammer drills etc. If you have any queries at all please contact us now.
+            </p>
+            <button class="btn btn-primary text-white">Get Started</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
