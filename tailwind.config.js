a296199d3a664df0f4d47041c92e6b4a6bd40738
Abdul-Rakib92 <rakibctgu@gmail.com>
2022-05-22 18:26:22 +0600

    tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "blue",
+          secondary: "#0FCFEC",
+          accent: "red",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
+  plugins: [require("daisyui")],
+}
